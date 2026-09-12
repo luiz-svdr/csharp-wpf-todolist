fa793444e29b8d80913936c47e3b1ec3026fcd99
Luiz G. Salvador <luizg.salvadoroliveira@gmail.com>
2026-09-11 21:00:16 -0300

    Adicionar arquivos de projeto.

---
+++ MyToDoList/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyToDoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddTask_Click(object sender, RoutedEventArgs e)
+        {
+            var newTaskWindow = new AddTaskWindow();
+            newTaskWindow.Owner = this;
+            newTaskWindow.ShowDialog();
+            string taskText = newTaskWindow.NewTask;
+
+            //TaskDisplay.Text = taskText;
+        }
+
+        private void DeleteTask_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MyToDoList/Views/AddTaskWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyToDoList
+{
+    /// <summary>
+    /// Lógica interna para AddTaskWindow.xaml
+    /// </summary>
+    public partial class AddTaskWindow : Window
+    {
+        public string NewTask { get; private set; }
+
+        public AddTaskWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTaskWindowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string newTask = AddTaskTextBox.Text;  
+            NewTask = newTask;
+            this.Close();       
+
+        }
+        private void CancelTaskWindowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
